12bfaad68fdf02ade343fa304fac92db5bf22f57
Kailash Dabhi <kailash09dabhi@gmail.com>
2017-09-19 21:39:58 +0530

    Implement Timezone api activity

--- app/src/main/java/com/worldweatheronline/ApiListActivity.java
+++ app/src/main/java/com/worldweatheronline/ApiListActivity.java
@@ -1,8 +1,8 @@
 package com.worldweatheronline;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
 
@@ -15,6 +15,11 @@ public final class ApiListActivity extends AppCompatActivity {
     findViewById(R.id.weatherButton).setOnClickListener(new OnClickListener() {
       @Override public void onClick(View view) {
         startActivity(new Intent(ApiListActivity.this, WeatherApiActivity.class));
+      }
+    });
+    findViewById(R.id.timezoneButton).setOnClickListener(new OnClickListener() {
+      @Override public void onClick(View view) {
+        startActivity(new Intent(ApiListActivity.this, TimezoneApiActivity.class));
       }
     });
   }
--- app/src/main/java/com/worldweatheronline/TimezoneApiActivity.java
+++ app/src/main/java/com/worldweatheronline/TimezoneApiActivity.java
@@ -1,9 +1,17 @@
 package com.worldweatheronline;
 
-import android.app.Activity;
+import android.app.SearchManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.TextView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import com.worldweatheronline.domain.entity.timezone.Api;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -14,14 +22,25 @@ import retrofit2.Response;
  * @date 19 Sep, 2017 13:05
  */
 
-public final class TimezoneApiActivity extends Activity {
+public final class TimezoneApiActivity extends AppCompatActivity {
+
+  @BindView(R.id.timezoneView) TextView timezoneView;
+  @BindView(R.id.nearestAreaView) TextView nearestAreaView;
 
   @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_timezone);
+    ButterKnife.bind(this);
+//    fetchTimezone("Paris");
+  }
 
-    App.instance().apiService().timezone("Paris").enqueue(new Callback<Api>() {
+  private void fetchTimezone(String location) {
+    App.instance().apiService().timezone(location).enqueue(new Callback<Api>() {
       @Override public void onResponse(Call<Api> call, Response<Api> response) {
-        Log.e("response", response.body().toString());
+        Api timezone = response.body();
+        if (timezone != null) {
+          timezoneView.setText(timezone.toString());
+        }
       }
 
       @Override public void onFailure(Call<Api> call, Throwable t) {
@@ -29,4 +48,37 @@ public final class TimezoneApiActivity extends Activity {
       }
     });
   }
+
+  public void hideKeyboard() {
+    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+    imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+  }
+
+  @Override public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.menu, menu);
+    final MenuItem item = menu.findItem(R.id.action_search);
+
+    SearchManager searchManager =
+        (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+    SearchView searchView =
+        (SearchView) item.getActionView();
+    searchView.setSearchableInfo(
+        searchManager.getSearchableInfo(getComponentName()));
+    searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+      @Override public boolean onQueryTextSubmit(String query) {
+        fetchTimezone(query);
+        return true;
+      }
+
+      @Override public boolean onQueryTextChange(String newText) {
+        return false;
+      }
+    });
+    searchView.setQuery("Paris", true);
+//    searchView.setIconifiedByDefault(false);
+//    searchView.setSubmitButtonEnabled(true);
+    searchView.setQueryHint("Search by city");
+    return true;
+  }
+
 }
